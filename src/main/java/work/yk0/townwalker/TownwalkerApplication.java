5dd7c86e87389bdbbb4a21eca623ca549e6e17ec
ykonno <konchan1129@gmail.com>
2019-09-10 18:25:54 +0900

    :tada: initial commit!

---
+++ src/main/java/work/yk0/townwalker/TownwalkerApplication.java
@@ -0,0 +1,13 @@
+package work.yk0.townwalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownwalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownwalkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/yk0/townwalker/TownwalkerApplicationTests.java
@@ -0,0 +1,16 @@
+package work.yk0.townwalker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TownwalkerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
