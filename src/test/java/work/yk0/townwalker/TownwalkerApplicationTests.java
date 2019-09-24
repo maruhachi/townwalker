36a96191cb5761f32edc693d85acfd1ba51f85e1
ykonno <konchan1129@gmail.com>
2019-09-24 16:14:29 +0900

    :sparkle: 君のAPはテストしてるかい?

--- src/test/java/work/yk0/townwalker/TownwalkerApplicationTests.java
+++ src/test/java/work/yk0/townwalker/TownwalkerApplicationTests.java
@@ -1,16 +1,32 @@
 package work.yk0.townwalker;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class TownwalkerApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	public void contextLoads() {
+	public void Bootが立ち上がりhomeが正しく表示されること() throws Exception{
+		this.mockMvc.perform(get("/")).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("予測不可能driving")));
 	}
 
 }
